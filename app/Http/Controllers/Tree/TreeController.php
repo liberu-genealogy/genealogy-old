89b30caaddc69d8b32fd9822dd8df35c7867b63f
Curtis Delicata <curtis.delicata@outlook.com>
2018-06-08 23:42:55 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/Tree/TreeController.php
+++ app/Http/Controllers/Tree/TreeController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers\Tree;
 
-use App\Individual;
 use App\Family;
+use App\Individual;
 use App\Http\Controllers\Controller;
 
 class TreeController extends Controller
@@ -15,8 +15,6 @@ class TreeController extends Controller
 
     public function links(Family $family)
     {
-        return $family = \DB::table('family_individual')->where('family_individual.type_id', '>=' , 1)->select(\DB::raw("family_individual.family_id as sid"), \DB::Raw("family_individual.individual_id as tid"))->get('sid', 'tid');
-
+        return $family = \DB::table('family_individual')->where('family_individual.type_id', '>=', 1)->select(\DB::raw('family_individual.family_id as sid'), \DB::Raw('family_individual.individual_id as tid'))->get('sid', 'tid');
     }
-
 }
