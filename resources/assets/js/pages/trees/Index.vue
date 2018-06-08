94788a8547e30972c75c2588c22db55efc79a7e6
Curtis Delicata <curtis.delicata@outlook.com>
2018-06-08 23:29:47 +0100

    Work in progress for tree pages

--- resources/assets/js/pages/trees/Index.vue
+++ resources/assets/js/pages/trees/Index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="svg-container">
+    <div class="parent-div">
     <d3-network ref="net" :net-nodes="nodes" :net-links="links" :options="options" />
     </div>
 </template>
@@ -52,25 +52,34 @@ export default {
 <style>
     .link { stroke: #0b86f3; }
 
+    .node { fill: #88c149;}
+
     .net-svg {
+        position: fixed;
+        z-index: 1;
+        padding-top: 100px;
+        left: 0;
+        top: 0;
         width: 100%;
         height: 100%;
+        overflow: auto;
+        color: black;
+        text-align: left;
     }
 
-    .svg-container {
-        display: inline-block;
-        position: relative;
-        width: 100%;
-        padding-bottom: 100%; /* aspect ratio */
-        vertical-align: top;
-        overflow: hidden;
-    }
-    .svg-content-responsive {
-        display: inline-block;
-        position: absolute;
-        top: 10px;
-        left: 0;
+    #parentDiv {
+        height: calc(100vh - 100px); /** output container is small for display */
+        width: calc(100vw - 100px);
+        display: block;
+        border: 1px solid red;
     }
 
-    .node { fill: #88c149;}
+    .modal-content {
+        background-color: #fefefe;
+        margin: auto;
+        padding: 20px;
+        border-radius: 3px;
+        border: 1px solid #888;
+        width: 60%;
+    }
 </style>
