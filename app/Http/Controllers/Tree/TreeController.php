161dd024798ce947233da25abe09e6c41d950418
Curtis Delicata <curtis.delicata@outlook.com>
2018-06-09 15:02:08 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/Tree/TreeController.php
+++ app/Http/Controllers/Tree/TreeController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Tree;
 
 use App\Individual;
-use App\Family;
 use App\Http\Controllers\Controller;
 
 class TreeController extends Controller
@@ -17,8 +16,7 @@ class TreeController extends Controller
     {
         //   return $family = \DB::table('family_individual')->where('family_individual.type_id', '>=' , 1)->select(\DB::raw("family_individual.family_id as sid"), \DB::Raw("family_individual.individual_id as tid"))->get('sid', 'tid');
 
-
-        /**
+        /*
          * return $family = \DB::table('family_individual')
          * ->leftJoin('individuals AS parents', function ($join) {
          * $join->on('parents.id', '=', 'family_individual.individual_id')
@@ -42,9 +40,6 @@ class TreeController extends Controller
 
 //        return $family->individuals()->select('individual_id')->get('individual_id');
 //        return $individual->parents()->select(\DB::raw("child_parent.child_id as sid", \DB::raw("child_parent.parent_id as tid")))->get('sid','tid');
-          return $individual = \DB::table('child_parent')->select(\DB::raw("child_id as sid"), (\DB::raw("parent_id as tid")))->get('sid','tid');
-
-
+        return $individual = \DB::table('child_parent')->select(\DB::raw('child_id as sid'), (\DB::raw('parent_id as tid')))->get('sid', 'tid');
     }
-
 }
