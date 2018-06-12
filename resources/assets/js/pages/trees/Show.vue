9c5b63114fdebb6d846627370c574aece846a9f3
Curtis Delicata <curtis.delicata@outlook.com>
2018-06-12 18:38:03 +0100

    Pedigree show

--- resources/assets/js/pages/trees/Show.vue
+++ resources/assets/js/pages/trees/Show.vue
@@ -1,45 +1,3 @@
-<template>
-    <div class="graph">
-        <hierarchical-edge-bundling identifier="id" :data="tree" :links="links" node-text="name" :smargin-x="20" :margin-y="20"/>
-    </div>
-</template>
-
-<script>
-import { hierarchicalEdgeBundling } from 'vued3tree';
-
-export default {
-    components: {
-        hierarchicalEdgeBundling,
-    },
-    data() {
-        return {
-            tree: {
-                name: 'father',
-                children: [{
-                    name: 'son1',
-                    children: [{ name: 'grandson', id: 1 }, { name: 'grandson2', id: 2 }],
-                }, {
-                    name: 'son2',
-                    children: [{ name: 'grandson3', id: 3 }, { name: 'grandson4', id: 4 }],
-                }],
-            },
-            links: [
-                { source: 3, target: 1, type: 1 },
-                { source: 3, target: 4, type: 2 },
-            ],
-            linkTypes: [
-                { id: 1, name: 'depends', symmetric: true },
-                {
-                    id: 2, name: 'implement', inName: 'implements', outName: 'is implemented by',
-                },
-                {
-                    id: 3, name: 'uses', inName: 'uses', outName: 'is used by',
-                },
-            ],
-        };
-    },
-};
-</script>
 
 <style>
     .graph .link {
@@ -82,3 +40,46 @@ export default {
         opacity: 0.1;
     }
 </style>
+
+<template>
+    <div class="graph">
+        <hierarchical-edge-bundling identifier="id" :data="tree" :links="links" :link-types="linkTypes" node-text="name" :smargin-x="20" :margin-y="20"/>
+    </div>
+</template>
+
+<script>
+import { hierarchicalEdgeBundling } from 'vued3tree';
+
+export default {
+    components: {
+        hierarchicalEdgeBundling,
+    },
+    data() {
+        return {
+            tree: {
+                name: 'father',
+                children: [{
+                    name: 'son1',
+                    children: [{ name: 'grandson', id: 1 }, { name: 'grandson2', id: 2 }],
+                }, {
+                    name: 'son2',
+                    children: [{ name: 'grandson3', id: 3 }, { name: 'grandson4', id: 4 }],
+                }],
+            },
+            links: [
+                { source: 3, target: 1, type: 1 },
+                { source: 3, target: 4, type: 2 },
+            ],
+            linkTypes: [
+                { id: 1, name: 'depends', symmetric: true },
+                {
+                    id: 2, name: 'implement', inName: 'implements', outName: 'is implemented by',
+                },
+                {
+                    id: 3, name: 'uses', inName: 'uses', outName: 'is used by',
+                },
+            ],
+        };
+    },
+};
+</script>
