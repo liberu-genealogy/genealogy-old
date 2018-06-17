9ba0050580a6c0feeafc594abdfa7d446514011a
Curtis Delicata <curtis.delicata@outlook.com>
2018-06-17 09:40:39 +0100

    Modify environment and naming

--- config/app.php
+++ config/app.php
@@ -11,7 +11,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => env('APP_NAME', 'Laravel Enso'),
+    'name' => env('APP_NAME', 'Modular Genealogy'),
 
     /*
     |--------------------------------------------------------------------------
