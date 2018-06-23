55e7f1808b2d55d36b81dd861ca3ac11b73f8d6e
Curtis Delicata <curtis.delicata@outlook.com>
2018-06-23 18:43:22 +0100

    Make event factory for individual only for now.

--- database/factories/EventFactory.php
+++ database/factories/EventFactory.php
@@ -12,15 +12,15 @@
  */
 
 $factory->define(App\Event::class, function (Faker\Generator $faker) {
-    $event = $faker->randomElement(['App\Individual', 'App\Family']);
+   // $event = $faker->randomElement(['App\Individual', 'App\Family']);
 
     return [
         'name'          => $faker->sentence,
         'description'   => $faker->text,
         'date'          => $faker->dateTimeBetween($startDate = '-200 years', $endDate = 'now'),
         'is_active'     => $faker->boolean,
-        'event_type'    => $event,
-        'event_id'      => $faker->unique()->randomNumber,
+        'event_type'    => 'App\Individual',
+        'event_id'      => $faker->randomElement(\App\Individual::pluck('id')->toArray()),
         'event_type_id' => $faker->randomElement(\App\EventType::pluck('id')->toArray()),
     ];
 });
