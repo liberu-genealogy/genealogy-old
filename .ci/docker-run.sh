d0fa4306aca420b92a4008f1a8b607fe33af0595
Curtis Delicata <curtis.delicata@outlook.com>
2018-06-29 01:17:46 +0100

    Travis Docker MySQL password.

--- .ci/docker-run.sh
+++ .ci/docker-run.sh
@@ -44,41 +44,6 @@ docker run -d \
   --secure-file-priv=/var/tmp \
   $MYSQL_EXTRA
 
-for i in `seq 1 30`; do
-	# wait for mysql to come up
-	sleep 1
-	# try running the init script
-	docker exec -it $NAME bash -c 'mysql -uroot -pexample < /etc/mysql/conf.d/init.sql' >/dev/null 2>&1
-	if [ $? -ne 0 ]; then continue; fi
-	if [[ $OMIT_FEATURES != *"Sha256Password"* ]]; then
-	 	docker exec -it $NAME bash -c 'mysql -uroot -pexample < /etc/mysql/conf.d/init_sha256.sql' >/dev/null 2>&1
-		if [ $? -ne 0 ]; then continue; fi
-	fi
-
-	if [[ $OMIT_FEATURES != *"CachingSha2Password"* ]]; then
-		docker exec -it $NAME bash -c 'mysql -uroot -pexample < /etc/mysql/conf.d/init_caching_sha2.sql' >/dev/null 2>&1
-		if [ $? -ne 0 ]; then continue; fi
-	fi
-
-	# exit if successful
-	docker exec -it $NAME mysql -uroot -pexample \
-		--ssl-mode=REQUIRED \
-		--ssl-ca=/etc/mysql/conf.d/certs/ssl-ca-cert.pem \
-		--ssl-cert=/etc/mysql/conf.d/certs/ssl-client-cert.pem \
-		--ssl-key=/etc/mysql/conf.d/certs/ssl-client-key.pem \
-		-e "SELECT 1"
-	if [ $? -ne 0 ]; then
-		# mariadb uses --ssl=TRUE instead of --ssl-mode=REQUIRED
-		docker exec -it $NAME mysql -uroot -pexample \
-			--ssl=TRUE \
-			--ssl-ca=/etc/mysql/conf.d/certs/ssl-ca-cert.pem \
-			--ssl-cert=/etc/mysql/conf.d/certs/ssl-client-cert.pem \
-			--ssl-key=/etc/mysql/conf.d/certs/ssl-client-key.pem \
-			-e "SELECT 1"
-		if [ $? -ne 0 ]; then
-			>&2 echo "Problem with SSL"
-			exit 1
-		fi
 	fi
 	echo "Ran Init Script"
 	exit 0
