29a56eb5403273f1ef89f058ce2a66f60d2d9c5c
Curtis Delicata <curtis.delicata@outlook.com>
2018-07-07 21:41:22 +0100

    Merge branch 'master' of https://github.com/laravel-enso/Enso
    # Conflicts:
    #	.env.example
    #	.travis.yml
    #	README.md
    #	app/Http/Controllers/Examples/Tables/Templates/exampleTable.json
    #	composer.lock
    #	database/seeds/DatabaseSeeder.php
    #	package.json
    #	public/0.js.map
    #	public/1.js
    #	public/1.js.map
    #	public/10.js
    #	public/10.js.map
    #	public/11.js
    #	public/11.js.map
    #	public/12.js
    #	public/12.js.map
    #	public/13.js
    #	public/13.js.map
    #	public/14.js
    #	public/14.js.map
    #	public/15.js
    #	public/15.js.map
    #	public/16.js
    #	public/16.js.map
    #	public/17.js
    #	public/17.js.map
    #	public/18.js
    #	public/18.js.map
    #	public/19.js
    #	public/19.js.map
    #	public/2.js.map
    #	public/20.js
    #	public/20.js.map
    #	public/21.js
    #	public/21.js.map
    #	public/22.js
    #	public/22.js.map
    #	public/23.js
    #	public/23.js.map
    #	public/24.js
    #	public/24.js.map
    #	public/25.js
    #	public/25.js.map
    #	public/26.js
    #	public/26.js.map
    #	public/27.js
    #	public/27.js.map
    #	public/28.js
    #	public/28.js.map
    #	public/29.js
    #	public/29.js.map
    #	public/3.js.map
    #	public/30.js
    #	public/30.js.map
    #	public/31.js
    #	public/31.js.map
    #	public/32.js.map
    #	public/33.js
    #	public/33.js.map
    #	public/34.js.map
    #	public/35.js.map
    #	public/36.js.map
    #	public/37.js.map
    #	public/38.js.map
    #	public/39.js.map
    #	public/4.js.map
    #	public/5.js
    #	public/5.js.map
    #	public/6.js
    #	public/6.js.map
    #	public/7.js
    #	public/7.js.map
    #	public/8.js
    #	public/8.js.map
    #	public/9.js
    #	public/9.js.map
    #	public/css/enso.css
    #	public/css/enso.css.map
    #	public/js/enso.js
    #	public/js/enso.js.map
    #	public/js/selectExample.js
    #	public/js/tableExample.js
    #	public/themes/cerulean/bulma.min.css
    #	public/themes/clean/bulma.min.css
    #	public/themes/cosmo/bulma.min.css
    #	public/themes/cyborg/bulma.min.css
    #	public/themes/darkly/bulma.min.css
    #	public/themes/flatly/bulma.min.css
    #	public/themes/journal/bulma.min.css
    #	public/themes/litera/bulma.min.css
    #	public/themes/lux/bulma.min.css
    #	public/themes/materia/bulma.min.css
    #	public/themes/minty/bulma.min.css
    #	public/themes/pulse/bulma.min.css
    #	public/themes/sandstone/bulma.min.css
    #	public/themes/simplex/bulma.min.css
    #	public/themes/slate/bulma.min.css
    #	public/themes/solar/bulma.min.css
    #	public/themes/spacelab/bulma.min.css
    #	public/themes/superhero/bulma.min.css
    #	public/themes/united/bulma.min.css
    #	public/themes/yeti/bulma.min.css
    #	resources/views/examples/table/partials/template.blade.php
    #	yarn.lock

--- resources/assets/js/pages/system/roles/Index.vue
+++ resources/assets/js/pages/system/roles/Index.vue
@@ -9,11 +9,7 @@
 
 <script>
 
-import fontawesome from '@fortawesome/fontawesome';
-import { faFileAlt, faSlidersH } from '@fortawesome/fontawesome-free-solid/shakable.es';
 import VueTable from '../../../components/enso/vuedatatable/VueTable.vue';
-
-fontawesome.library.add([faFileAlt, faSlidersH]);
 
 export default {
     components: { VueTable },
