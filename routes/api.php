59328e7d29a2585b7dc067a30e895b2be191cd0e
Curtis Delicata <curtis@delicata.eu>
2018-11-03 17:54:02 +0000

    Begin events and individiuals generated structure. Ready for migrating of existing stucture.

--- routes/api.php
+++ routes/api.php
@@ -2,5 +2,43 @@
 
 Route::middleware(['auth', 'core'])
     ->group(function () {
-        // add your local routes
+Route::namespace('Events')
+    ->prefix('events')->as('events.')
+    ->group(function () {
+       Route::get('initTable', 'EventsTableController@init')
+           ->name('initTable');
+       Route::get('tableData', 'EventsTableController@data')
+           ->name('tableData');
+       Route::get('exportExcel', 'EventsTableController@excel')
+           ->name('exportExcel');
+
+       Route::get('options', 'EventsSelectController@options')
+           ->name('options');
+   });
+
+Route::namespace('Events')
+
+    ->group(function () {
+        Route::resource('events', 'EventsController'); // if it's the case, use `except` or `only` to avoid generating unused routes
+    });
+Route::namespace('Individuals')
+    ->prefix('individuals')->as('individuals.')
+    ->group(function () {
+       Route::get('initTable', 'IndividualsTableController@init')
+           ->name('initTable');
+       Route::get('tableData', 'IndividualsTableController@data')
+           ->name('tableData');
+       Route::get('exportExcel', 'IndividualsTableController@excel')
+           ->name('exportExcel');
+
+       Route::get('options', 'IndividualsSelectController@options')
+           ->name('options');
+   });
+
+Route::namespace('Individuals')
+
+    ->group(function () {
+        Route::resource('individuals', 'IndividualsController'); // if it's the case, use `except` or `only` to avoid generating unused routes
+    });
+
     });
