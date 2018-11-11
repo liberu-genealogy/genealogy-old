6638453e6ea35f36db19359aee168eee1126e824
Curtis Delicata <curtis.delicata@outlook.com>
2018-11-11 15:58:56 +0000

    Tree menu

---
+++ database/migrations/2018_11_11_151400_create_structure_for_trees_parent_menu.php
@@ -0,0 +1,11 @@
+<?php
+
+use LaravelEnso\StructureManager\app\Classes\StructureMigration;
+
+class CreateStructureForTreesParentMenu extends StructureMigration
+{
+    protected $menu = [
+        'name' => 'Trees', 'icon' => 'book', 'route' => null, 'order_index' => 999, 'has_children' => true
+    ];
+}
+
