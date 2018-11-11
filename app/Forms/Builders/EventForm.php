51903c21c2fb496e6507ae2de3eb8a8bf96d69fb
Curtis Delicata <curtis.delicata@outlook.com>
2018-11-11 14:07:22 +0000

    Event form

--- app/Forms/Builders/EventForm.php
+++ app/Forms/Builders/EventForm.php
@@ -3,6 +3,7 @@
 namespace App\Forms\Builders;
 
 use App\Event;
+use App\EventType;
 use LaravelEnso\FormBuilder\app\Classes\Form;
 
 class EventForm
@@ -18,11 +19,16 @@ class EventForm
 
     public function create()
     {
-        return $this->form->create();
+        return $this->form->options('event_type_id', EventType::with('events')->get(['event_types.name', 'event_types.id']))->title('Insert')
+            ->create();
     }
 
     public function edit(Event $event)
     {
-        return $this->form->edit($event);
+        return $this->form->options('event_type_id', EventType::with('events')->get(['event_types.name', 'event_types.id']))
+            //   ->options('individual_id', Event::with('individuals')->get(['individuals.first_name', 'individuals.id']))
+            ->title('Edit')
+            ->actions(['update'])
+            ->edit($event);
     }
 }
