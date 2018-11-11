ecbf83d1c78e6bf2369b4f0193392f7e75871558
Curtis Delicata <curtis.delicata@outlook.com>
2018-11-11 14:01:42 +0000

    Fix Family Form Builder

--- app/Forms/Builders/FamilyForm.php
+++ app/Forms/Builders/FamilyForm.php
@@ -3,6 +3,8 @@
 namespace App\Forms\Builders;
 
 use App\Family;
+use App\Individual;
+use App\Enums\IndividualTypes;
 use LaravelEnso\FormBuilder\app\Classes\Form;
 
 class FamilyForm
@@ -18,11 +20,24 @@ class FamilyForm
 
     public function create()
     {
-        return $this->form->create();
+        return $this->form
+            ->options('type_id', IndividualTypes::select())
+            ->options('individualList', Individual::select('individuals.*', \DB::raw("CONCAT(individuals.first_name, ' ', individuals.last_name)  AS name"), 'individuals.id')->get(['name', 'individuals.id']))
+            ->options('mother_id', Individual::get(['first_name', 'individuals.id']))
+            ->options('father_id', Individual::get(['first_name', 'individuals.id']))
+            ->create();
     }
 
     public function edit(Family $family)
     {
-        return $this->form->edit($family);
+        $family->append(['individualList']);
+
+        return $this->form
+            ->options('type_id', IndividualTypes::select())
+            ->options('individualList', Individual::select('individuals.*', \DB::raw("CONCAT(individuals.first_name, ' ', individuals.last_name)  AS name"), 'individuals.id')->get(['name', 'individuals.id']))
+            ->options('mother_id', Individual::get(['first_name', 'individuals.id']))
+            ->options('father_id', Individual::get(['first_name', 'individuals.id']))
+            ->append('family_id', $family->id)
+            ->edit($family);
     }
 }
