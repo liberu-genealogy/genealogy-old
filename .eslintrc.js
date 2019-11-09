9ad3aa4ea58e0a0022d1853778e0078c9bdf1bb8
mishu <mishu@xtelecom.ro>
2019-11-09 20:00:47 +0200

    stable

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,39 @@
 module.exports = {
-    'root': true,
+    root: true,
     extends: [
         'airbnb-base',
-        'plugin:vue/recommended'
+        'plugin:vue/recommended',
     ],
     plugins: [
         'vue',
     ],
     globals: {
         axios: true,
-        route: true,
+    },
+    settings: {
+        'import/resolver': {
+            alias: {
+                map: [
+                    ['@core', `${__dirname}/node_modules/@enso-ui/ui/src`],
+                    ['@calendar', `${__dirname}/node_modules/@enso-ui/calendar/src/bulma`],
+                    ['@financials', `${__dirname}/node_modules/@enso-ui/financials/src/bulma`],
+                ],
+            },
+        },
     },
     parserOptions: {
         parser: 'babel-eslint',
         sourceType: 'module',
-        'ecmaVersion': 2017,
-        allowImportExportEverywhere: true
+        ecmaVersion: 2017,
+        allowImportExportEverywhere: true,
     },
     rules: {
         'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
         indent: ['error', 4],
         'vue/html-indent': 0,
-        'vue/attributes-order': 0,
+        // 'vue/attributes-order': 0,
         'no-plusplus': 0,
-        'no-debugger': 0,
         'no-param-reassign': 0,
         'no-mixed-operators': 0,
         'func-names': 0,
@@ -32,13 +41,13 @@ module.exports = {
         'vue/max-attributes-per-line': 0,
         'no-return-assign': ['error', 'except-parens'],
         'vue/html-closing-bracket-newline': ['error', {
-            'singleline': 'never',
-            'multiline': 'never'
+            singleline: 'never',
+            multiline: 'never',
         }],
         'vue/html-closing-bracket-spacing': ['error', {
-            'startTag': 'never',
-            'endTag': 'never',
-            'selfClosingTag': 'never'
-        }]
+            startTag: 'never',
+            endTag: 'never',
+            selfClosingTag: 'never',
+        }],
     },
 };
