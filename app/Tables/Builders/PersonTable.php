2ded13bc98afd9bf8d0794202d7cd81e3ab659e2
Curtis Delicata <curtis.delicata@outlook.com>
2020-06-09 22:58:12 +0000

    Fix persons table

--- app/Tables/Builders/PersonTable.php
+++ app/Tables/Builders/PersonTable.php
@@ -6,7 +6,7 @@ use App\Person;
 use Illuminate\Database\Eloquent\Builder;
 use LaravelEnso\Tables\App\Contracts\Table;
 
-class PersonTable implements Table
+class PersonTable extends \LaravelEnso\People\App\Tables\Builders\PersonTable
 {
     protected const TemplatePath = __DIR__.'/../Templates/people.json';
 
