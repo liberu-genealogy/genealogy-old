624b8024c2183d5d9ce3a8704b4d67785004cad4
Curtis Delicata <curtis.delicata@outlook.com>
2020-06-11 19:32:49 +0000

    Make name nullable

--- database/migrations/2020_06_03_192100_add_columns_for_people_table.php
+++ database/migrations/2020_06_03_192100_add_columns_for_people_table.php
@@ -11,6 +11,7 @@ class AddColumnsForPeopleTable extends Migration
         Schema::table('people', function (Blueprint $table) {
             $table->string('givn');
             $table->text('surn')->nullable();
+	    $table->string('name')->nullable()->change();
             $table->char('sex', 1)->nullable();
             $table->text('description')->nullable();
             $table->integer('child_in_family_id')->references('id')->on('families')->nullable();
@@ -27,6 +28,7 @@ class AddColumnsForPeopleTable extends Migration
 	    $table->string('bank_account');
             $table->dropColumn('givn');
             $table->dropColumn('surn');
+	    $table->string('name')->nullable(false)->change();
             $table->dropColumn('sex');
             $table->dropColumn('description');
             $table->dropColumn('child_in_family_id');
