526f5932aa8412432053d1f0c30d7912cc63a35f
jyyblue <wenqiangliu344@gmail.com>
2020-06-12 12:47:49 -0700

    add subn and subm in import

---
+++ app/Subm.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property integer $id
+ * @property string $subm
+ * @property string $name
+ * @property string $addr
+ * @property string $rin
+ * @property string $rfn
+ * @property string $lang
+ * @property string $phon
+ * @property string $created_at
+ * @property string $updated_at
+ */
+class Subm extends Model
+{
+    /**
+     * The "type" of the auto-incrementing ID.
+     * 
+     * @var string
+     */
+    protected $keyType = 'integer';
+
+    /**
+     * @var array
+     */
+    protected $fillable = ['subm', 'name', 'addr', 'rin', 'rfn', 'lang', 'phon', 'created_at', 'updated_at'];
+
+}
