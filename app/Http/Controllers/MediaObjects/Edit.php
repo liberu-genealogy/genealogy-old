ac81b7e1f6acb2271c577bb69b398454fdccaa2a
Curtis Delicata <curtis.delicata@outlook.com>
2020-06-18 22:18:38 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/MediaObjects/Edit.php
+++ app/Http/Controllers/MediaObjects/Edit.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers\MediaObjects;
 
+use App\Forms\Builders\MediaObjectForm;
 use App\MediaObject;
 use Illuminate\Routing\Controller;
-use App\Forms\Builders\MediaObjectForm;
 
 class Edit extends Controller
 {
