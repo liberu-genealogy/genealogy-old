60edd848df7387a89a263de59a2bed1673806d64
Curtis Delicata <curtis.delicata@outlook.com>
2020-06-30 01:41:37 +0100

    Validate requests for the new pages.

--- app/Http/Requests/ValidateFamilySlgsRequest.php
+++ app/Http/Requests/ValidateFamilySlgsRequest.php
@@ -13,8 +13,14 @@ class ValidateFamilySlgsRequest extends FormRequest
 
     public function rules()
     {
+        $familyslgs = $this->route('familyslgs');
         return [
-            //
+
+            'family_id' => 'required|max:50',
+            'stat' => 'required|max:50',
+            'date' => 'required|max:50',
+            'plac' => 'required|max:50',
+            'temp' => 'required|max:50'
         ];
     }
 }
