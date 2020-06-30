ec2fef3d5767b46bdfb14b99d0d098549de64919
Curtis Delicata <curtis.delicata@outlook.com>
2020-06-30 02:28:06 +0100

    Refactor the menu links to organise them more and keep number of links per parent menu down to manageable amount.

--- database/migrations/2020_04_10_154603_create_structure_for_sources_parent.php
+++ database/migrations/2020_04_10_154603_create_structure_for_sources_parent.php
@@ -5,6 +5,6 @@ use LaravelEnso\Migrator\App\Database\Migration;
 class CreateStructureForSourcesParent extends Migration
 {
     protected $menu = [
-        'name' => 'Persons', 'icon' => 'book', 'route' => null, 'order_index' => 792, 'has_children' => true,
+        'name' => 'Sources', 'icon' => 'book', 'route' => null, 'order_index' => 792, 'has_children' => true,
     ];
 }
