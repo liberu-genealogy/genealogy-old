fdd72c6e57884e0be37fa2bb80b72094cd350ecc
jyyblue <wenqiangliu344@gmail.com>
2020-06-30 04:57:17 -0700

    add register and email verify page

---
+++ resources/lang/en/emails.php
@@ -0,0 +1,48 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Emails Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for various emails that
+    | we need to display to the user. You are free to modify these
+    | language lines according to your application's requirements.
+    |
+    */
+
+    /*
+     * Activate new user account email.
+     *
+     */
+
+    'activationSubject'  => 'Activation required',
+    'activationGreeting' => 'Welcome!',
+    'activationMessage'  => 'You need to activate your email before you can start using all of our services.',
+    'activationButton'   => 'Activate',
+    'activationThanks'   => 'Thank you for using our application!',
+
+     /*
+     * Activate new c user account email.
+     *
+     */
+
+    'activationSubjectApi'  => 'Activation required',
+    'activationGreetingApi' => 'Welcome!',
+    'activationMessageApi'  => 'You need to activate your email before you can start using all of our services.',
+    'activationButtonApi'   => 'Activate',
+    'activationThanksApi'   => 'Thank you for using our application!',
+
+    /*
+     * Goobye email.
+     *
+     */
+    'goodbyeSubject'  => 'Sorry to see you go...',
+    'goodbyeGreeting' => 'Hello :username,',
+    'goodbyeMessage'  => 'We are very sorry to see you go. We wanted to let you know that your account has been deleted. Thank for the time we shared. You have '.config('settings.restoreUserCutoff').' days to restore your account.',
+    'goodbyeButton'   => 'Restore Account',
+    'goodbyeThanks'   => 'We hope to see you again!',
+
+];
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Route::middleware(['web'])->group(
+//     function() {
+        Route::post('register', '\App\Http\Controllers\Auth\RegisterController@register');
+        Route::post('verify', '\App\Http\Controllers\Auth\VerificationController@verify_user');
+//     }
+// );
+
 // example data for the dashboard
 Route::middleware(['web', 'auth'])
     ->namespace('Dashboard')
