8aea5be27fc27760797c9bcff0a0c770d657bcb5
jyyblue <wenqiangliu344@gmail.com>
2020-07-01 05:07:24 -0700

    working multitenant structure

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -1,19 +1,124 @@
 <?php
 
+// namespace App\Http\Controllers\Auth;
+
+// use App\Http\Controllers\Controller;
+// use App\Providers\RouteServiceProvider;
+// use Illuminate\Foundation\Auth\AuthenticatesUsers;
+
+// class LoginController extends Controller
+// {
+//     use AuthenticatesUsers;
+
+//     protected $redirectTo = RouteServiceProvider::HOME;
+
+//     public function __construct()
+//     {
+//         $this->middleware('guest')->except('logout');
+//     }
+// }
+
+
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+use LaravelEnso\Core\App\Events\Login;
+use LaravelEnso\Core\App\Models\User;
+use LaravelEnso\Multitenancy\App\Services\Tenant;
+use LaravelEnso\Multitenancy\App\Enums\Connections;
 
 class LoginController extends Controller
 {
     use AuthenticatesUsers;
 
-    protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo = '/';
 
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+
+        $this->maxAttempts = config('enso.auth.maxLoginAttempts');
+    }
+
+    public function logout(Request $request)
+    {
+        $this->guard()->logout();
+
+        $request->session()->invalidate();
+    }
+
+    protected function attemptLogin(Request $request)
+    {
+        $user = $this->loggableUser($request);
+
+        if (! $user) {
+            return false;
+        }
+
+        Auth::login($user, $request->input('remember'));
+
+        Login::dispatch($user, $request->ip(), $request->header('User-Agent'));
+
+        return true;
+    }
+
+    protected function authenticated(Request $request, $user)
+    {
+        return response()->json([
+            'auth' => Auth::check(),
+            'csrfToken' => csrf_token(),
+        ]);
+    }
+
+    private function loggableUser(Request $request)
+    {
+        $user = User::whereEmail($request->input('email'))->first();
+        $company = $user->company();
+        $tanent = false;
+        if($company) {
+            $tanent = true;
+        }
+
+        if ($company->isTenant()) {
+            // $key = 'database.default';
+            // $value = Connections::Tenant;
+            // config([$key => $value]);
+
+
+            // Tenant::set($company);
+            $value = Connections::Tenant.$company->id;
+
+        }else{
+            $value = '';
+
+        }
+            $key = 'database.connections.mysql.database';
+            config([$key => $value]);
+    
+            \DB::purge('mysql');
+    
+            \DB::reconnect('mysql');
+
+            \Session::put('db', $value);
+
+        if (! optional($user)->currentPasswordIs($request->input('password'))) {
+            return;
+        }
+
+        if ($user->passwordExpired()) {
+            throw ValidationException::withMessages([
+                'email' => 'Password expired. Please set a new one.',
+            ]);
+        }
+        if ($user->isInactive()) {
+            throw ValidationException::withMessages([
+                'email' => 'Account disabled. Please contact the administrator.',
+            ]);
+        }
+        return $user;
     }
 }
