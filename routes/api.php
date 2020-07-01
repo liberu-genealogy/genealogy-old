0b78812212cfa6bb9e37fd69141c14588d5eed9a
Curtis Delicata <curtis.delicata@outlook.com>
2020-07-01 19:12:21 +0100

    Routes

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,21 @@ Route::namespace('\LaravelEnso\Core\Http\Controllers')
     ->group(function () {
         Route::get('/meta', 'Guest')->name('meta');
 
-        Route::middleware(['api', 'auth'])
+        Route::namespace('Auth')
+            ->middleware('api')
+            ->group(function () {
+                Route::middleware('guest')->group(function () {
+                    Route::post('login', 'LoginController@login')->name('login');
+                    Route::post('password/email', 'ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+                    Route::post('password/reset', 'ResetPasswordController@attemptReset')->name('password.reset');
+                });
+
+                Route::middleware('auth')->group(function () {
+                    Route::post('logout', 'LoginController@logout')->name('logout');
+                });
+            });
+
+        Route::middleware(['api ', 'auth'])
             ->group(fn () => Route::get('/sentry', 'Sentry')->name('sentry'));
 
         Route::middleware(['api', 'auth', 'core'])
