b8999ca8510c9fd6d79c6f787664ca13b6321402
Curtis Delicata <curtis.delicata@outlook.com>
2020-07-05 18:11:23 +0100

    Change title and meta tags for Facebook using stub and changing vue build config.

--- client/vue.config.js
+++ client/vue.config.js
@@ -9,7 +9,7 @@ module.exports = {
                 ? '../resources/views/index.blade.php'
                 : './index.html',
             template: process.env.NODE_ENV === 'production'
-                ? '../vendor/laravel-enso/core/stubs/production-index.blade.stub'
+                ? './src/stubs/production-index.blade.stub'
                 : '../vendor/laravel-enso/core/stubs/development-index.stub',
         },
     },
