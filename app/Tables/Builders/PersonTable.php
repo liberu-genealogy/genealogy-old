78456c49a0cba563e8745ba0a031027f8b98709f
Curtis Delicata <curtis.delicata@outlook.com>
2020-10-03 14:37:36 +0100

    Fixes for PersonForm.php and PersonTable.php Update routes to overwrite and add multitenant.

--- app/Tables/Builders/PersonTable.php
+++ app/Tables/Builders/PersonTable.php
@@ -3,22 +3,18 @@
 namespace App\Tables\Builders;
 
 use App\Person;
-use App\Traits\ConnectionTrait;
 use Illuminate\Database\Eloquent\Builder;
 use LaravelEnso\Tables\Contracts\Table;
 
 class PersonTable extends \LaravelEnso\People\Tables\Builders\PersonTable
 {
-    use ConnectionTrait;
 
     protected const TemplatePath = __DIR__.'/../Templates/people.json';
 
     public function query(): Builder
     {
-        $conn = $this->getConnection();
-        $db = $this->getDB();
 
-        return Person::on($conn)->selectRaw('
+        return Person::selectRaw('
             people.id
             , people.title, people.givn, people.surn,  people.appellative, people.email, people.phone,
             people.birthday, people.deathday, CASE WHEN users.id is null THEN 0 ELSE 1 END as "user",
--- routes/api.php
+++ routes/api.php
@@ -683,3 +683,56 @@ Route::middleware(['api', 'auth', 'core', 'multitenant'])
                 Route::get('{personSubm}', 'Show')->name('show');
             });
     });
+
+Route::namespace('\LaravelEnso\People\Http\Controllers')
+    ->middleware(['api', 'auth', 'core','multitenant'])
+    ->prefix('api/administration/people')
+    ->as('administration.people.')
+    ->group(function () {
+        Route::get('create', 'Create')->name('create');
+        Route::post('', 'Store')->name('store');
+        Route::get('{person}/edit', 'Edit')->name('edit');
+        Route::patch('{person}', 'Update')->name('update');
+        Route::delete('{person}', 'Destroy')->name('destroy');
+
+        Route::get('initTable', 'InitTable')->name('initTable');
+        Route::get('tableData', 'TableData')->name('tableData');
+        Route::get('exportExcel', 'ExportExcel')->name('exportExcel');
+
+        Route::get('options', 'Options')->name('options');
+    });
+
+Route::namespace('\LaravelEnso\Companies\Http\Controllers')
+    ->middleware(['api', 'auth', 'core','multitenant'])
+    ->prefix('api/administration/companies')
+    ->as('administration.companies.')
+    ->group(function () {
+        Route::namespace('Company')
+            ->group(function () {
+                Route::get('create', 'Create')->name('create');
+                Route::post('', 'Store')->name('store');
+                Route::get('{company}/edit', 'Edit')->name('edit');
+                Route::patch('{company}', 'Update')->name('update');
+                Route::delete('{company}', 'Destroy')->name('destroy');
+
+                Route::get('initTable', 'InitTable')->name('initTable');
+                Route::get('tableData', 'TableData')->name('tableData');
+                Route::get('exportExcel', 'ExportExcel')->name('exportExcel');
+
+                Route::get('options', 'Options')->name('options');
+            });
+        Route::namespace('Person')
+            ->group(function () {
+                Route::prefix('people')
+                    ->as('people.')
+                    ->group(function () {
+                        Route::get('{company}', 'Index')->name('index');
+                        Route::get('{company}/create', 'Create')->name('create');
+                        Route::get('{company}/{person}/edit', 'Edit')->name('edit');
+                        Route::patch('{person}', 'Update')->name('update');
+                        Route::post('', 'Store')->name('store');
+                        Route::delete('{company}/{person}', 'Destroy')->name('destroy');
+                    });
+            });
+
+    });
