614877578835ae7504a3f484be8be4465d8701af
Curtis Delicata <curtis.delicata@outlook.com>
2020-10-03 14:54:23 +0100

    Fix LoginController.php if missing company

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use LaravelEnso\Core\Events\Login;
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use LaravelEnso\Companies\Models\Company;
 use App\Traits\ConnectionTrait;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
@@ -73,39 +74,39 @@ class LoginController extends Controller
         }
 
 //        if ($main_company == null && !$user->isAdmin())
-          if ($main_company == null)
-        {
-            $company_count = Company::count();
+          if ($main_company == null) {
+              $company_count = Company::count();
 
-            $company = Company::create([
-                'name' => $user->email . ($company_count + 1),
-                'email' => $user->email,
-                // 'is_active' => 1,
-                'is_tenant' => 1,
-                'status' => 1,
-            ]);
-            $user->person->companies()->attach($company->id, ['person_id' => $user->person->id, 'is_main' => 1, 'is_mandatary' => 1, 'company_id' => $company->id]);
-
-/**            Tree::create([
-                'name' => 'Default Tree',
-                'description' => 'Automatically created tree as only tree remaining was deleted.',
-                'user_id' => $user->id,
-                'company_id' => $company->id,
-            ]);
-*/
-            $company_id = $company->id;
-            $user_id = $user->id;
-            $person_name = $user->person->name;
-            $user_email = $user->email;
-
-            $db = $company_id;
-            $this->setConnection(Connections::Tenant, $db, $user_id);
-            $this->getConnection();
-
-            CreateDB::dispatch($company, $user_id);
-            Migration::dispatch($company_id, $user_id, $person_name, $user_email);
+              $company = Company::create([
+                  'name' => $user->email . ($company_count + 1),
+                  'email' => $user->email,
+                  // 'is_active' => 1,
+                  'is_tenant' => 1,
+                  'status' => 1,
+              ]);
+              $user->person->companies()->attach($company->id, ['person_id' => $user->person->id, 'is_main' => 1, 'is_mandatary' => 1, 'company_id' => $company->id]);
 
 
+              /**            Tree::create([
+               * 'name' => 'Default Tree',
+               * 'description' => 'Automatically created tree as only tree remaining was deleted.',
+               * 'user_id' => $user->id,
+               * 'company_id' => $company->id,
+               * ]);
+               */
+              $company_id = $company->id;
+              $user_id = $user->id;
+              $person_name = $user->person->name;
+              $user_email = $user->email;
+
+              $db = $company_id;
+              $this->setConnection(Connections::Tenant, $db, $user_id);
+              $this->getConnection();
+
+              CreateDB::dispatch($company, $user_id);
+              Migration::dispatch($company_id, $user_id, $person_name, $user_email);
+
+          }
             if (! optional($user)->currentPasswordIs($request->input('password'))) {
             return;
         }
