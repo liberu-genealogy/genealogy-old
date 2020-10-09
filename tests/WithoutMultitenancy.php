0288f7d1ea60ad73e63bc267fbc3f7e8a71846c4
Curtis Delicata <curtis.delicata@outlook.com>
2020-10-09 22:05:43 +0100

    New attempt at fixing build with ignoring multitenancy.

--- tests/WithoutMultitenancy.php
+++ tests/WithoutMultitenancy.php
@@ -1,23 +1,23 @@
 <?php
 
-namespace Tests;
+    namespace Tests;
 
-use Exception;
-use LaravelEnso\Multitenancy\Http\Middleware\Multitenancy;
+    use Exception;
+    use \LaravelEnso\Multitenancy\Http\Middleware\Multitenancy;
 
-trait WithoutMultitenancy
-{
-    /**
-     * Prevent all middleware from being executed for this test class.
-     *
-     * @throws \Exception
-     */
-    public function disableMiddlewareForAllTests()
+    trait WithoutMultitenancy
     {
-        if (method_exists($this, 'withoutMiddleware')) {
-            $this->withoutMiddleware([Multitenancy::class]);
-        } else {
-            throw new Exception('Unable to disable middleware. MakesHttpRequests trait not used.');
+        /**
+         * Prevent all middleware from being executed for this test class.
+         *
+         * @throws \Exception
+         */
+        public function disableMiddlewareForAllTests()
+        {
+            if (method_exists($this, 'withoutMiddleware')) {
+                $this->withoutMiddleware([Multitenancy::class]);
+            } else {
+                throw new Exception('Unable to disable middleware. MakesHttpRequests trait not used.');
+            }
         }
     }
-}
