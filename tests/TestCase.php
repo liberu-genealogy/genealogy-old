4cf6556e082fcafe216e00bb540c80db83d1846e
Curtis Delicata <curtis.delicata@outlook.com>
2020-10-09 20:39:55 +0100

    Use WithoutMiddleware for tests and update composer.lock

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,10 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    use WithoutMiddleware;
 }
