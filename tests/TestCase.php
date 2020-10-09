fb3cfb223dd0d7a64078e3e2270e709edbcf1bd4
Curtis Delicata <curtis.delicata@outlook.com>
2020-10-09 21:23:29 +0100

    Try without only Multitenancy for tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -9,4 +9,11 @@ abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
     use WithoutMiddleware;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->withoutMiddleware([\LaravelEnso\Multitenancy\Http\Middleware\Multitenancy::class]);
+    }
+
 }
