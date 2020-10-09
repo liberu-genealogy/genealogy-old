12a7e9c74018ca3c59893c66ffc0d7c12bdea43c
Curtis Delicata <curtis.delicata@outlook.com>
2020-10-09 23:02:23 +0100

    Another attempt.

--- tests/WithoutMultitenancy.php
+++ tests/WithoutMultitenancy.php
@@ -3,9 +3,12 @@
     namespace Tests;
 
     use Exception;
+    use Illuminate\Foundation\Testing\Concerns\MakesHttpRequests;
 
     trait WithoutMultitenancy
     {
+        use MakesHttpRequests;
+
         /**
          * Prevent all middleware from being executed for this test class.
          *
@@ -14,7 +17,7 @@
         public function disableMiddlewareForAllTests()
         {
             if (method_exists($this, 'withoutMiddleware')) {
-                $this->withoutMiddleware(\LaravelEnso\Multitenancy\Http\Middleware\Multitenancy::class);
+                $this->withoutMiddleware([\LaravelEnso\Multitenancy\Http\Middleware\Multitenancy::class]);
             } else {
                 throw new Exception('Unable to disable middleware. MakesHttpRequests trait not used.');
             }
