797cb9ccbba4bdaf2a708571db62222788c65bbb
Curtis Delicata <curtisdelicata@users.noreply.github.com>
2020-10-16 18:28:16 +0000

    Apply fixes from StyleCI

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,14 +2,14 @@
 
 namespace Database\Seeders;
 
+use CompanySeeder;
+use CustomPermissionSeeder;
 use Illuminate\Database\Seeder;
 use LaravelEnso\Core\Database\Seeders\UserGroupSeeder;
 use LaravelEnso\Core\Database\Seeders\UserSeeder;
 use LaravelEnso\Countries\Database\Seeders\CountrySeeder;
 use LaravelEnso\Localisation\Database\Seeders\LanguageSeeder;
 use LaravelEnso\Roles\Database\Seeders\RoleSeeder;
-use CompanySeeder;
-use CustomPermissionSeeder;
 
 class DatabaseSeeder extends Seeder
 {
