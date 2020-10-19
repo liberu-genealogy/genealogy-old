ca7adf7ccb437349ba85fa578d56606960717d6e
Curtis Delicata <curtis.delicata@outlook.com>
2020-10-19 14:31:15 +0100

    Only run tenancy code in LoginController.php if not running unit tests.

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -87,50 +87,53 @@ class LoginController extends Controller
     private function loggableUser(Request $request)
     {
         $user = User::whereEmail($request->input('email'))->first();
-        $company = $user->company();
-        $tanent = false;
-        if ($company) {
-            $tanent = true;
-        }
-        // set company id as default
-        $main_company = $user->person->company();
-        if ($main_company !== null && ! ($user->isAdmin())) {
-            $c_id = $main_company->id;
-            $db = Connections::Tenant.$c_id;
-            $this->setConnection(Connections::Tenant, $db);
-        }
 
-        if ($main_company == null && ! $user->isAdmin()) {
+        if (! App::runningUnitTests()) {
+            $company = $user->company();
+            $tanent = false;
+            if ($company) {
+                $tanent = true;
+            }
+            // set company id as default
+            $main_company = $user->person->company();
+            if ($main_company !== null && !($user->isAdmin())) {
+                $c_id = $main_company->id;
+                $db = Connections::Tenant . $c_id;
+                $this->setConnection(Connections::Tenant, $db);
+            }
+
+            if ($main_company == null && !$user->isAdmin()) {
 //          if ($main_company == null) {
-            $company_count = Company::count();
+                $company_count = Company::count();
 
-            $company = Company::create([
-                'name' => $user->email.($company_count + 1),
-                'email' => $user->email,
-                // 'is_active' => 1,
-                'is_tenant' => 1,
-                'status' => 1,
-            ]);
-            $user->person->companies()->attach($company->id, ['person_id' => $user->person->id, 'is_main' => 1, 'is_mandatary' => 1, 'company_id' => $company->id]);
+                $company = Company::create([
+                    'name' => $user->email . ($company_count + 1),
+                    'email' => $user->email,
+                    // 'is_active' => 1,
+                    'is_tenant' => 1,
+                    'status' => 1,
+                ]);
+                $user->person->companies()->attach($company->id, ['person_id' => $user->person->id, 'is_main' => 1, 'is_mandatary' => 1, 'company_id' => $company->id]);
 
-            /**            Tree::create([
-             * 'name' => 'Default Tree',
-             * 'description' => 'Automatically created tree as only tree remaining was deleted.',
-             * 'user_id' => $user->id,
-             * 'company_id' => $company->id,
-             * ]);.
-             */
-            $company_id = $company->id;
-            $user_id = $user->id;
-            $person_name = $user->person->name;
-            $user_email = $user->email;
+                /**            Tree::create([
+                 * 'name' => 'Default Tree',
+                 * 'description' => 'Automatically created tree as only tree remaining was deleted.',
+                 * 'user_id' => $user->id,
+                 * 'company_id' => $company->id,
+                 * ]);.
+                 */
+                $company_id = $company->id;
+                $user_id = $user->id;
+                $person_name = $user->person->name;
+                $user_email = $user->email;
 
-            $db = $company_id;
-            $this->setConnection(Connections::Tenant, $db, $user_id);
-            $this->getConnection();
+                $db = $company_id;
+                $this->setConnection(Connections::Tenant, $db, $user_id);
+                $this->getConnection();
 
-            CreateDB::dispatch($company, $user_id);
-            Migration::dispatch($company_id, $user_id, $person_name, $user_email);
+                CreateDB::dispatch($company, $user_id);
+                Migration::dispatch($company_id, $user_id, $person_name, $user_email);
+            }
         }
         if (! optional($user)->currentPasswordIs($request->input('password'))) {
             return;
