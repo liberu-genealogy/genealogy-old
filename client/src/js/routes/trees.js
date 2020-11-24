df21ce3046782c1554d58788b39b6d5192c3e298
Curtis Delicata <curtis.delicata@outlook.com>
2020-11-24 18:28:23 +0000

    Changes for latest Enso including new patches.

--- client/src/js/routes/trees.js
+++ client/src/js/routes/trees.js
@@ -1,7 +1,7 @@
-import routeImporter from '@core/modules/importers/routeImporter';
+import routeImporter from '@enso-ui/ui/src/modules/importers/routeImporter';
 
 const routes = routeImporter(require.context('./trees', false, /.*\.js$/));
-const RouterView = () => import('@core/bulma/pages/Router.vue');
+const RouterView = () => import('@enso-ui/ui/src/bulma/pages/Router.vue');
 
 export default {
     path: '/trees',
