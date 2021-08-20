5ce5332c71a0a567744f9601fede7c7f857475b9
Curtis Delicata <curtis.delicata@outlook.com>
2021-08-20 17:19:06 +0000

    Changes for meta and facebook

--- client.nuxt/nuxt.config.js
+++ client.nuxt/nuxt.config.js
@@ -1,32 +1,76 @@
+import getSiteMeta from "./utils/getSiteMeta";
+
+const meta = getSiteMeta();
+
+
 export default {
     ssr: false,
 
+  server: {
+    host: process.env.HOST ||'localhost' // default: localhost
+  },
+
+  publicRuntimeConfig: {
+    baseURL: process.env.BASE_URL || 'http://localhost:8000',
+    appEnv: process.env.APP_ENV || 'production'
+  },
+
+  sitemap: [
+    {
+      hostname: process.env.BASE_URL || 'http://localhost:3000',
+      path: '/sitemap.xml',
+      gzip: true,
+    }
+
+  ],
     // Global page headers: https://go.nuxtjs.dev/config-head
-    head: {
-        title: "nuxt-enso-ui",
-        htmlAttrs: {
-            lang: "en",
-        },
-        meta: [
-            { charset: "utf-8" },
-            {
-                name: "viewport",
-                content: "width=device-width, initial-scale=1",
-            },
-            { hid: "description", name: "description", content: "" },
-            { name: "format-detection", content: "telephone=no" },
-        ],
-        link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+  // Global page headers (https://go.nuxtjs.dev/config-head)
+  head: {
+    htmlAttrs: {
+      lang: "en-GB",
     },
+    title: "Family Tree 365 - Start your family tree today - free! Your first tree is 100% free. Sign-up to begin your genealogy journey today!",
+    meta: [
+      ...meta,
+      {charset: "utf-8"},
+      {name: "HandheldFriendly", content: "True"},
+      {name: "viewport", content: "width=device-width, initial-scale=1"},
+      {property: "og:site_name", content: "Family Tree 365"},
+      {
+        hid: "description",
+        name: "description",
+        content:
+          "Our user-friendly yet powerful platform lets you create your own family tree the quick and easy way. No technical knowledge is required. Start your family tree today - free!",
+      },
+      {property: "og:image:width", content: "2500"},
+      {property: "og:image:height", content: "780"},
+      {name: "twitter:site", content: "@familytree365"},
+      {name: "twitter:card", content: "summary_large_image"},
+    ],
+    link: [
+      {rel: "icon", href: "/favicon.ico"},
+      {
+        hid: "canonical",
+        rel: "canonical",
+        href: process.env.BASE_URL,
+      },
+    ],
 
     // Global CSS: https://go.nuxtjs.dev/config-css
     css: [],
+
+  env: {
+    STRIPE_KEY: process.env.STRIPE_KEY,
+    BASE_URL: process.env.BASE_URL,
+    ECHO_PORT: process.env.ECHO_PORT,
+  },
 
     // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
     plugins: [
         "~/plugins/bootEnums.js",
         "~/plugins/i18n.js",
         "~/plugins/pRoute.js",
+	"~/plugins/vue-fb-customer-chat.js",
     ],
 
     // Auto import components: https://go.nuxtjs.dev/config-components
@@ -37,6 +81,18 @@ export default {
         // https://go.nuxtjs.dev/eslint
         "@nuxtjs/eslint-module",
     ],
+  
+nuxtContentAlgolia: {
+    appId: process.env.ALGOLIA_APP_ID,
+    apiKey: process.env.ALGOLIA_API_KEY,
+    paths: [
+      {
+        name: 'articles',
+        index: 'articles',
+        fields: ['title', 'description', 'bodyPlainText']
+      }
+    ]
+  },
 
     // Modules: https://go.nuxtjs.dev/config-modules
     modules: [
