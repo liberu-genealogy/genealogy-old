0db41196221e6288b36b8273784fd203adfe6bec
Curtis Delicata <curtis.delicata@outlook.com>
2021-08-22 14:11:09 +0100

    Update TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,11 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->withoutMiddleware(\App\Http\Middleware\Multitenant::class);
+    }
 }
