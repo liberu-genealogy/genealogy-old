ac717d95bd9f185bf7d71d61dfde3b3cb57b0977
Curtis Delicata <curtis.delicata@outlook.com>
2021-08-22 14:50:43 +0100

    Update Person.php

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -3,7 +3,9 @@
 namespace App\Models;
 
 use LaravelEnso\People\Models\Person as CorePerson;
+use LaravelEnso\Multitenancy\Traits\SystemConnection;
 
 class Person extends CorePerson
 {
+    use SystemConnection;
 }
