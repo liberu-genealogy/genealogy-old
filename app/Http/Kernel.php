c70febe8271bbfc7c1a24b5bd69afd0d0f05f9a6
prihutomo <prihutomoh@gmail.com>
2021-08-24 01:58:59 +0800

    fix route middleware multitalent

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -47,6 +47,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'multitenant' => \App\Http\Middleware\Multitenant::class,
     ];
 
     protected $middlewarePriority = [
