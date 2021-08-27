a7b3d5a45c317e8a6544f02fb2f4b0f99e226e50
Heru Firmansyah <heru0502@gmail.com>
2021-08-27 17:53:48 +0800

    Fix not filled for birt dati, birt plac, deat dati, deat plac, deat caus, buri dati, and buri plac when import gedcom file

---
+++ database/migrations/tenant/2021_08_26_222539_add_column_dati_plac_famc_fams_to_people_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnDatiPlacFamcFamsToPeopleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('people', function (Blueprint $table) {
+            $table->string('birthday_dati')->nullable();
+            $table->string('birthday_plac')->nullable();
+            $table->string('deathday_dati')->nullable();
+            $table->string('deathday_plac')->nullable();
+            $table->string('deathday_caus')->nullable();
+            $table->string('burial_day_dati')->nullable();
+            $table->string('burial_day_plac')->nullable();
+            $table->string('famc')->nullable();
+            $table->string('fams')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('people', function (Blueprint $table) {
+            $table->dropColumn([
+                'birthday_dati',
+                'birthday_plac',
+                'deathday_dati',
+                'deathday_plac',
+                'deathday_caus',
+                'burial_day_dati',
+                'burial_day_plac',
+                'famc',
+                'fams'
+            ]);
+        });
+    }
+}
