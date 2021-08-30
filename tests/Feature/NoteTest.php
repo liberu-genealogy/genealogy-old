3f3e7b5a3201c5fe068f981aca5c40db8092ecf7
Kamlesh DjKammu <dhundharakamlesh@gmail.com>
2021-08-30 16:25:59 +0530

    Test For Node

---
+++ tests/Feature/NoteTest.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use LaravelEnso\Forms\TestTraits\CreateForm;
+use LaravelEnso\Forms\TestTraits\DestroyForm;
+use LaravelEnso\Forms\TestTraits\EditForm;
+use LaravelEnso\Tables\Traits\Tests\Datatable;
+use LaravelEnso\Users\Models\User;
+use App\Models\Note;
+use Tests\TestCase;
+class NodeTest extends TestCase {
+
+    use Datatable, DestroyForm, CreateForm, EditForm, RefreshDatabase;
+
+    private $permissionGroup = 'notes';
+    private $testModel;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->seed()
+            ->actingAs(User::first());
+
+        $this->testModel = Note::factory()->make();
+    }
+
+    /** @test */
+    public function can_view_create_form()
+    {
+        $this->get(route($this->permissionGroup.'.create', false))
+            ->assertStatus(200)
+            ->assertJsonStructure(['form']);
+    }
+
+    /** @test */
+    public function can_store_note()
+    {
+        $response = $this->post(
+            route('notes.store', [], false),
+            $this->testModel->toArray() + []
+        );
+
+        $note = Note::where('name', $this->testModel->name)->first();
+
+        $response->assertStatus(200)
+            ->assertJsonStructure(['message'])
+            ->assertJsonFragment([
+                'redirect' => 'notes.edit',
+                'param' => ['note' => $note->id],
+            ]);
+    }
+
+    /** @test */
+    public function can_update_note()
+    {
+        $this->testModel->save();
+
+        $this->testModel->name = 'updated';
+
+        $this->patch(
+            route('notes.update', $this->testModel->id, false),
+            $this->testModel->toArray() + []
+        )->assertStatus(200)
+            ->assertJsonStructure(['message']);
+
+        $this->assertEquals('updated', $this->testModel->fresh()->name);
+    }
+
+    /** @test */
+    public function get_option_list()
+    {
+        $this->testModel->save();
+
+        $this->get(route('notes.options', [
+            'query' => $this->testModel->name,
+            'limit' => 10,
+        ], false))
+            ->assertStatus(200)
+            ->assertJsonFragment(['name' => $this->testModel->name]);
+    }
+}
