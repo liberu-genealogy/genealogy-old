aa6fcbf49d41915ba50950e5d0e1209930885523
prihutomo <prihutomoh@gmail.com>
2021-09-01 02:18:44 +0800

    make daboville format

--- app/Http/Controllers/Families/Index.php
+++ app/Http/Controllers/Families/Index.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers\Families;
 
+use App\Models\Family;
+use App\Models\FamilyEvent;
+use App\Models\Person;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 
@@ -9,6 +12,15 @@ class Index extends Controller
 {
     public function __invoke(Request $request)
     {
-        //
+        return $this->getDabolvilleFormat();
+    }
+
+    public function getDabolvilleFormat()
+    {
+        //1 Progenitor - $husband married $woman $year $location
+        //1.1 Child - $husband married $woman $year $location
+            //1.1.1 Grandchild - $husband married $woman $year $location
+        $result = "true";
+        return $result;
     }
 }
