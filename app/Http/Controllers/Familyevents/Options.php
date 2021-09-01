9ab0af02e536e30545bdb32d1522e9b82f28035c
prihutomo <prihutomoh@gmail.com>
2021-09-02 01:46:11 +0800

    unfinished family event

--- app/Http/Controllers/Familyevents/Options.php
+++ app/Http/Controllers/Familyevents/Options.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Familyevents;
 
-use App\FamilyEvent;
+use App\Models\FamilyEvent;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use LaravelEnso\Select\Traits\OptionsBuilder;
@@ -13,7 +13,7 @@ class Options extends Controller
 
     protected string $model = FamilyEvent::class;
 
-    //protected $queryAttributes = ['name'];
+    protected $queryAttributes = ['title'];
 
     //public function query(Request $request)
     //{
