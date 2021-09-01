bb02963453516c06b74b8dd58a40dc1fbbad2015
prihutomo <prihutomoh@gmail.com>
2021-09-02 00:06:31 +0800

    fix chan test n code

--- app/Http/Controllers/Chan/Index.php
+++ app/Http/Controllers/Chan/Index.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers\Chan;
 
+use App\Models\Chan;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 
 class Index extends Controller
 {
-    public function __invoke(Request $request)
+    public function __invoke(Request $request, Chan $chan)
     {
-        //
+        return ['chans' => $chan];
     }
 }
