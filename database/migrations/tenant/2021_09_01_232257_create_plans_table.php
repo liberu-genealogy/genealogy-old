1b94755595ae5429c35bbe4243d425c14849d610
Heru Firmansyah <heru0502@gmail.com>
2021-09-02 04:28:37 +0800

    Crate migration table plans for paypal

---
+++ database/migrations/tenant/2021_09_01_232257_create_plans_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('plans', function (Blueprint $table) {
+            $table->id();
+            $table->string('plan_id')->nullable();
+            $table->string('status')->nullable();
+            $table->string('description')->nullable();
+            $table->string('usage_type')->nullable();
+            $table->dateTime('create_time')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('plans');
+    }
+}
