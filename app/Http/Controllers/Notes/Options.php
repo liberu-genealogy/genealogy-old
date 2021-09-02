ee9b6def52847119e63c5d5fc7f72b749142d2ad
prihutomo <prihutomoh@gmail.com>
2021-09-03 00:10:10 +0800

    fix note test and code

--- app/Http/Controllers/Notes/Options.php
+++ app/Http/Controllers/Notes/Options.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Notes;
 
-use App\Note;
+use App\Models\Note;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use LaravelEnso\Select\Traits\OptionsBuilder;
