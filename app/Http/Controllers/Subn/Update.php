935fad235cbd7c3edbdea4510912083b3dcbee91
prihutomo <prihutomoh@gmail.com>
2021-09-03 00:24:47 +0800

    fix subn test and code

--- app/Http/Controllers/Subn/Update.php
+++ app/Http/Controllers/Subn/Update.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Subn;
 
 use App\Http\Requests\ValidateSubnRequest;
-use App\Subn;
+use App\Models\Subn;
 use Illuminate\Routing\Controller;
 
 class Update extends Controller
