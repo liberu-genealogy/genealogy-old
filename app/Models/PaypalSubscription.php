375ed6b0a6e9fb0b9b9769214466a466d2a92841
Heru Firmansyah <heru0502@gmail.com>
2021-09-03 03:12:01 +0800

    Fix get plan in paypal

--- app/Models/PaypalSubscription.php
+++ app/Models/PaypalSubscription.php
@@ -5,9 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Plan extends Model
+class PaypalSubscription extends Model
 {
     use HasFactory;
-
-    protected $guarded = ['id'];
 }
