4c3f74004182cd214b47e372a981300e929a0000
Heru Firmansyah <heru0502@gmail.com>
2021-09-07 23:47:32 +0800

    Fix subscribe paypal

--- app/Http/Controllers/Paypal/HandlePayment.php
+++ app/Http/Controllers/Paypal/HandlePayment.php
@@ -20,40 +20,19 @@ class HandlePayment extends Controller
      */
     public function __invoke(Request $request)
     {
-        $pp = new PaypalSubscription($this->app_id, $this->app_sk, $this->mode);
-
-        $startdate = Carbon::now();
-        $startdate->addMinutes(1);
+        $pp = new PaypalSubscription();
 
         $subscription = [
-            'start_time' =>  $startdate->toAtomString(),
             'quantity' => '1',
-            'shipping_amount' => [
-                'currency_code' => 'GBP',
-                'value' => '0'
-            ],
             'subscriber' => [
                 'name' => [
-                    'given_name' => $request->input('first_name'),
-                    'surname' => $request->input('last_name')
+                    'given_name' => $request->first_name,
+                    'surname' => $request->last_name
                 ],
-                'email_address' => $request->input('email'),
-                'shipping_address' => [
-                    'name' => [
-                        'full_name' => $request->input('first_name').$request->input('last_name')
-                    ],
-                    'address' => [
-                        'address_line_1' => '2211 N First Street',
-                        'address_line_2' => 'Building  17',
-                        'admin_area_2' => 'San Jose',
-                        'admin_area_1' => 'CA',
-                        'postal_code' => '95131',
-                        'country_code' => 'US'
-                    ]
-                ]
+                'email_address' => $request->email,
             ],
             'application_context' => [
-                'brand_name' => 'Racks',
+                'brand_name' => 'Family Tree 365',
                 'locale' => 'es-ES',
                 'shipping_preference' => 'SET_PROVIDED_ADDRESS',
                 'user_action' => 'SUBSCRIBE_NOW',
@@ -63,7 +42,6 @@ class HandlePayment extends Controller
                 ],
                 'return_url' => 'http://127.0.0.1:3000/success',
                 'cancel_url' => 'http://127.0.0.1:3000/cancel'
-
             ]
         ];
 
@@ -74,7 +52,7 @@ class HandlePayment extends Controller
         $response = $pp->createSubscription($subscription, $plan);
 
         PaypalSub::create([
-            'paypal_subscription_id' => $response['id'],
+            'paypal_id' => $response['id'],
             'user_email' => $request->email,
             'paypal_plan_id' => $request->plan_id,
             'status' => $response['status'],
