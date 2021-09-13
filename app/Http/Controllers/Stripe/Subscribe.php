ac18dd9a9b943f961fa6a6f62e648158ebc8e8f4
Heru Firmansyah <heru0502@gmail.com>
2021-09-14 02:57:18 +0800

    Add middleware web to api stripe

--- app/Http/Controllers/Stripe/Subscribe.php
+++ app/Http/Controllers/Stripe/Subscribe.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Stripe;
 
 use App\Http\Controllers\Controller;
-use App\Models\User;
+use App\Notifications\SubscribeSuccessfully;
 use Illuminate\Http\Request;
 
 class Subscribe extends Controller
