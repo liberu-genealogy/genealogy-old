ae94c58f4ca4350f451d072eb852e595fdb3abd5
prihutomo <prihutomoh@gmail.com>
2021-09-15 00:21:01 +0800

    prevention edit user

--- app/Http/Controllers/Users/Edit.php
+++ app/Http/Controllers/Users/Edit.php
@@ -10,6 +10,11 @@ class Edit extends Controller
 {
     public function __invoke(User $user, UserForm $form)
     {
-        return ['form' => $form->edit($user)];
+        $auth = \Auth::user()->id;
+
+        if($auth === 1){
+            return ['form' => $form->edit($user)];
+        }
+        return ['error' => __('Unauthorized')];
     }
 }
--- app/Http/Controllers/Users/Update.php
+++ app/Http/Controllers/Users/Update.php
@@ -16,29 +16,34 @@ class Update extends Controller
 
     public function __invoke(ValidateUserRequest $request, User $user)
     {
-        $this->authorize('handle', $user);
+        $auth = \Auth::user()->id;
 
-        if ($request->filled('password')) {
-            $this->authorize('change-password', $user);
-            $user->password = bcrypt($request->get('password'));
+        if($auth === 1){
+            $this->authorize('handle', $user);
+
+            if ($request->filled('password')) {
+                $this->authorize('change-password', $user);
+                $user->password = bcrypt($request->get('password'));
+            }
+
+            $user->fill($request->validated());
+
+            if ($user->isDirty('group_id')) {
+                $this->authorize('change-group', $user);
+            }
+
+            if ($user->isDirty('role_id')) {
+                $this->authorize('change-role', $user);
+            }
+
+            $user->save();
+
+            if ((new Collection($user->getChanges()))->keys()->contains('password')) {
+                Event::dispatch(new PasswordReset($user));
+            }
+
+            return ['message' => __('The user was successfully updated')];
         }
-
-        $user->fill($request->validated());
-
-        if ($user->isDirty('group_id')) {
-            $this->authorize('change-group', $user);
-        }
-
-        if ($user->isDirty('role_id')) {
-            $this->authorize('change-role', $user);
-        }
-
-        $user->save();
-
-        if ((new Collection($user->getChanges()))->keys()->contains('password')) {
-            Event::dispatch(new PasswordReset($user));
-        }
-
-        return ['message' => __('The user was successfully updated')];
+        return ['error' => __('The user was unsuccessfully updated')];
     }
 }
