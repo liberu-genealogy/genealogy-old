3c2c057ecdf01b3f66ad7d4549037947e55b343e
prihutomo <prihutomoh@gmail.com>
2021-09-14 22:53:35 +0800

    add test user restrict

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -12,19 +12,31 @@ class UserSeeder extends Seeder
 {
     public function run()
     {
-        $person = $this->person();
+        $admin = $this->admin();
+        $supervisor = $this->supervisor();
 
         User::factory()->create([
-            'person_id' => $person->id,
+            'person_id' => $admin->id,
             'group_id' => UserGroup::whereName('Administrators')->first()->id,
-            'email' => $person->email,
+            'email' => $admin->email,
             'password' => '$2y$10$06TrEefmqWBO7xghm2PUzeF/O0wcawFUv8TKYq.NF6Dsa0Pnmd/F2',
             'role_id' => Role::whereName('admin')->first()->id,
             'is_active' => true,
         ])->generateAvatar();
+
+
+
+        User::factory()->create([
+            'person_id' => $supervisor->id,
+            'group_id' => UserGroup::whereName('Administrators')->first()->id,
+            'email' => $supervisor->email,
+            'password' => '$2y$10$06TrEefmqWBO7xghm2PUzeF/O0wcawFUv8TKYq.NF6Dsa0Pnmd/F2',
+            'role_id' => Role::whereName('supervisor')->first()->id,
+            'is_active' => true,
+        ])->generateAvatar();
     }
 
-    private function person()
+    private function admin()
     {
         return Person::factory()->create([
             'name' => 'Admin Root',
@@ -32,6 +44,17 @@ class UserSeeder extends Seeder
             'email' => 'admin@familytree365.com',
             'birthday' => '1970-01-01',
             'phone' => '+4412345678910',
+        ]);
+    }
+
+    private function supervisor()
+    {
+        return Person::factory()->create([
+            'name' => 'Supervisor',
+            'appellative' => 'Supervisor',
+            'email' => 'supervisor@familytree365.com',
+            'birthday' => '1970-01-01',
+            'phone' => '+4412345678911',
         ]);
     }
 }
