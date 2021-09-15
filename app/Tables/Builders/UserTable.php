2175945d4c2bed0273571a6397b461310400278d
prihutomo <prihutomoh@gmail.com>
2021-09-15 22:08:03 +0800

    change from user_id to role_id

--- app/Tables/Builders/UserTable.php
+++ app/Tables/Builders/UserTable.php
@@ -14,8 +14,10 @@ class UserTable implements Table
 
     public function query(): Builder
     {
-        $auth = \Auth::user()->id;
-        if($auth === 1) {
+        $role = \Auth::user()->role_id;
+        $userId = \Auth::user()->id;
+
+        if(in_array($role, [1,2])) {
             $user = User::with('avatar:id,user_id')->selectRaw('
                 users.id, user_groups.name as "group", people.name, people.appellative,
                 people.phone, users.email, roles.name as role, users.is_active,
@@ -30,7 +32,7 @@ class UserTable implements Table
                 users.created_at, users.person_id
             ')->join('people', 'users.person_id', '=', 'people.id')
                 ->join('user_groups', 'users.group_id', '=', 'user_groups.id')
-                ->join('roles', 'users.role_id', '=', 'roles.id')->where('users.id', $auth);
+                ->join('roles', 'users.role_id', '=', 'roles.id')->where('users.id', $userId);
         }
 
         return $user;
