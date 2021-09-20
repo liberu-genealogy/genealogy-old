33f41e80c9572b2647231b0735aa1c5e505ed5f2
webstar1027 <webstar1027@gmail.com>
2021-09-20 05:02:41 +0200

    login-issue

--- config/session.php
+++ config/session.php
@@ -155,7 +155,7 @@ return [
     |
     */
 
-    'domain' => env('SESSION_DOMAIN', null),
+    'domain' => env('SESSION_DOMAIN', 'localhost'),
 
     /*
     |--------------------------------------------------------------------------
