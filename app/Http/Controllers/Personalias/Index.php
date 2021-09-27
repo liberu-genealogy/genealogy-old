01eae301d3589f3e4bc2b5c3966a67e5794de6e5
webstar1027 <webstar1027@gmail.com>
2021-09-27 14:52:32 +0200

    get person api implementation

--- app/Http/Controllers/Personalias/Index.php
+++ app/Http/Controllers/Personalias/Index.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers\Personalias;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use App\Models\Person;
+use App\Service\Tenant;
+use LaravelEnso\Multitenancy\Enums\Connections;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class Index extends Controller
 {
@@ -15,7 +19,11 @@ class Index extends Controller
 
     public function getPerson()
     {
-        $persons = Person::all();
-        return $persons;
+        $tenant = Auth::user()->company();
+        Tenant::set($tenant);
+        $company = Tenant::get();
+        $db = Connections::Tenant.$company->id;
+        $person = DB::connection(Connections::Tenant)->table('people')->get();
+        return $person;
     }
 }
