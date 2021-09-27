6d8fa153ce07cbe12be488b6335a251bed74b55f
webstar1027 <webstar1027@gmail.com>
2021-09-27 02:42:30 +0200

    get person api

--- app/Http/Controllers/Personalias/Index.php
+++ app/Http/Controllers/Personalias/Index.php
@@ -4,11 +4,18 @@ namespace App\Http\Controllers\Personalias;
 
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use App\Models\Person;
 
 class Index extends Controller
 {
     public function __invoke(Request $request)
     {
         //
+    }
+
+    public function getPerson()
+    {
+        $persons = Person::all();
+        return $persons;
     }
 }
