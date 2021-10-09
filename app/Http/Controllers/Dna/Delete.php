b3667516bb53f54bd8fbe27d51b25c46066f6c71
Curtis Delicata <curtis.delicata@outlook.com>
2021-10-09 18:25:41 +0100

    Update Delete.php

--- app/Http/Controllers/Dna/Delete.php
+++ app/Http/Controllers/Dna/Delete.php
@@ -9,11 +9,21 @@ class Destroy extends Controller
 {
     public function __invoke(Dna $addr)
     {
-        $addr->delete();
+                
+        $user = auth()->user();
+        $dna = Dna::find($id);
+        if ($user->id == $dna->user_id) {
+            $dna->delete();
 
-        return [
-            'message' => __('The DNA was successfully deleted'),
-            'redirect' => 'dna.index',
-        ];
+            return [
+                'message' => __('The dna was successfully deleted'),
+                'redirect' => 'dna.index',
+            ];
+        } else {
+            return [
+                'message' => __('The dna could not be deleted'),
+                'redirect' => 'dna.index',
+            ];
+        }
     }
 }
