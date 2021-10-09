8e81f7ecab5bfcb822259ae62c218e045118191d
Curtis Delicata <curtis.delicata@outlook.com>
2021-10-09 18:24:40 +0100

    Update Update.php

--- app/Http/Controllers/Dna/Update.php
+++ app/Http/Controllers/Dna/Update.php
@@ -1,17 +1,17 @@
 <?php
 
-namespace App\Http\Controllers\Addrs;
+namespace App\Http\Controllers\Dna;
 
 use App\Http\Requests\ValidateAddrRequest;
-use App\Models\Addr;
+use App\Models\Dna;
 use Illuminate\Routing\Controller;
 
 class Update extends Controller
 {
-    public function __invoke(ValidateAddrRequest $request, Addr $addr)
+    public function __invoke(ValidateAddrRequest $request, Dna $dna)
     {
-        $addr->update($request->validated());
+        $dna->update($request->validated());
 
-        return ['message' => __('The addr was successfully updated')];
+        return ['message' => __('The DNA was successfully updated')];
     }
 }
