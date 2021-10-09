2174275b949a25d67f1264100a8488e9c0cd60ff
Curtis Delicata <curtis.delicata@outlook.com>
2021-10-09 18:14:27 +0100

    Create Store.php

---
+++ app/Http/Controllers/Dna/Store.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Jobs\DnaMatching;
+use App\Models\Dna;
+use Auth;
+use Illuminate\Http\Request;
+
+public function store(Request $request)
+    {
+        if ($request->hasFile('file')) {
+            if ($request->file('file')->isValid()) {
+                try {
+                    $currentUser = Auth::user();
+                    $file_name = 'dna_'.$request->file('file')->getClientOriginalName().uniqid().'.'.$request->file('file')->extension();
+                    $request->file->storeAs('dna', $file_name);
+                    define('STDIN', fopen('php://stdin', 'r'));
+                    $random_string = unique_random('dnas', 'variable_name', 5);
+                    $var_name = 'var_'.$random_string;
+                    $filename = 'app/dna/'.$file_name;
+                    $user_id = $currentUser->id;
+                    $dna = new Dna();
+                    $dna->name = 'DNA Kit for user '.$user_id;
+                    $dna->user_id = $user_id;
+                    $dna->variable_name = $var_name;
+                    $dna->file_name = $file_name;
+                    $dna->save();
+                    DnaMatching::dispatch($currentUser, $var_name, $file_name);
+
+                    return [
+                        'message' => __('The dna was successfully created'),
+                        'redirect' => 'dna.edit',
+                        'param' => ['dna' => $dna->id],
+                    ];
+                } catch (\Exception $e) {
+                    return $e->getMessage();
+                }
+            }
+
+            return ['File corrupted'];
+        }
+
+        return response()->json(['Not uploaded'], 422);
+    }
+
+}
