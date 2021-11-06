94fa04026185399f194e33e2424fdcc29619a1d3
Curtis Delicata <curtis.delicata@outlook.com>
2021-11-06 08:57:38 +0000

    Create Login.php

---
+++ app/Traits/Login.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace LaravelEnso\Core\Traits;
+
+use Illuminate\Http\Request;
+
+trait Login
+{
+    public function login(Request $request)
+    {
+        $this->validateLogin($request);
+
+        if (
+            method_exists($this, 'hasTooManyLoginAttempts') &&
+            $this->hasTooManyLoginAttempts($request)
+        ) {
+            $this->fireLockoutEvent($request);
+
+            return $this->sendLockoutResponse($request);
+        }
+
+        if ($this->attemptLogin($request)) {
+            if ($request->attributes->get('sanctum')) {
+                $request->session()->put('auth.password_confirmed_at', time());
+            }
+
+            return $this->sendLoginResponse($request);
+        }
+
+        $this->incrementLoginAttempts($request);
+
+        return $this->sendFailedLoginResponse($request);
+    }
+}
