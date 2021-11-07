4abd3e612b6ddabc7892a8947d01c2e5f54a01ee
Curtis Delicata <curtis.delicata@outlook.com>
2021-11-07 21:29:23 +0000

    Fix

--- app/Forms/Builders/FamilyForm.php
+++ app/Forms/Builders/FamilyForm.php
@@ -4,7 +4,7 @@ namespace App\Forms\Builders;
 
 use App\Models\Family;
 use App\Models\Person;
-use App\Models\Type;
+// use App\Models\Type;
 use LaravelEnso\Forms\Services\Form;
 
 class FamilyForm
@@ -33,7 +33,6 @@ class FamilyForm
         return $this->form
         ->options('husband_id', Person::all())
         ->options('wife_id', Person::all())
-        ->options('type_id', Type::all())
         ->append('family_id', $family->id)
          ->value('child_id', $family->children)
          ->options('child_id', Person::all())
