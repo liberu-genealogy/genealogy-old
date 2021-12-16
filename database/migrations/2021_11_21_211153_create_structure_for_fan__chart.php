77e1078e447747f5059acfa0844c65aac35ac7ab
shahghasiadil <shaghasy555@gmail.com>
2021-12-16 14:42:40 +0430

    committed code

--- database/migrations/2021_11_21_211153_create_structure_for_fan__chart.php
+++ database/migrations/2021_11_21_211153_create_structure_for_fan__chart.php
@@ -7,16 +7,16 @@ use Illuminate\Support\Facades\Schema;
 class CreateStructureForFanChart extends Migration
 {
     protected array $permissions = [
-        ['name' => 'tree.index', 'description' => 'Show index for tree', 'is_default' => true],
+        // ['name' => 'tree.index', 'description' => 'Show index for tree', 'is_default' => true],
 
-        ['name' => 'tree.show', 'description' => 'show tree', 'is_default' => true],
-        ['name' => 'tree.store', 'description' => 'Store a new tree', 'is_default' => true],
+        ['name' => 'fanchart.show', 'description' => 'show tree', 'is_default' => true],
+        ['name' => 'fanchart.index', 'description' => 'Store a new tree', 'is_default' => true],
 
     ];
 
     protected array $menu = [
         // 'name' => 'Pidegree-chart', 'icon' => 'users', 'route' => 'tree.show', 'order_index' => 999, 'has_children' => false,
-        'name' => 'Fan-chart', 'icon' => 'users', 'route' => 'tree.show', 'order_index' => 999, 'has_children' => false,
+        'name' => 'Fan-chart', 'icon' => 'users', 'route' => 'fanchart.index', 'order_index' => 999, 'has_children' => false,
         // 'name' => 'Decendent-chart', 'icon' => 'users', 'route' => 'tree.show', 'order_index' => 999, 'has_children' => false,
     ];
 
