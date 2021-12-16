4b50e4a2a7357e4f1d6bbc400a96797fcaa22363
shahghasiadil <shaghasy555@gmail.com>
2021-12-16 14:42:40 +0430

    committed code

--- database/migrations/2021_11_21_210944_create_structure_for_pedigree_chart.php
+++ database/migrations/2021_11_21_210944_create_structure_for_pedigree_chart.php
@@ -9,13 +9,12 @@ class CreateStructureForPedigreeChart extends Migration
     protected array $permissions = [
         ['name' => 'pedigree.index', 'description' => 'Show index for tree', 'is_default' => true],
 
-        ['name' => 'tree.show', 'description' => 'show tree', 'is_default' => true],
-        ['name' => 'tree.store', 'description' => 'Store a new tree', 'is_default' => true],
+        ['name' => 'pedigree.show', 'description' => 'show tree', 'is_default' => true],
 
     ];
 
     protected array $menu = [
-        'name' => 'pedigree-chart', 'icon' => 'users', 'route' => 'tree.show', 'order_index' => 999, 'has_children' => false,
+        'name' => 'pedigree-chart', 'icon' => 'users', 'route' => 'pedigree.show', 'order_index' => 999, 'has_children' => false,
         // 'name' => 'Fan-chart', 'icon' => 'users', 'route' => 'tree.show', 'order_index' => 999, 'has_children' => false,
         // 'name' => 'Decendent-chart', 'icon' => 'users', 'route' => 'tree.show', 'order_index' => 999, 'has_children' => false,
     ];
