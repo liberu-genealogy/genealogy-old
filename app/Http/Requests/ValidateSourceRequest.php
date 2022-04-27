a21976324717129559a2755e1121255d4bbb2303
Curtis Delicata <curtis.delicata@outlook.com>
2022-04-27 14:34:06 +0100

    Update ValidateSourceRequest.php

--- app/Http/Requests/ValidateSourceRequest.php
+++ app/Http/Requests/ValidateSourceRequest.php
@@ -23,7 +23,7 @@ class ValidateSourceRequest extends FormRequest
             'repository_id' => 'required|exists:repositories,id',
             'author_id' => 'required|max:10',
             'publication_id' => 'required|max:10',
-            'type_id' => 'required|max:10',
+            // 'type_id' => 'required|max:10',
         ];
     }
 }
