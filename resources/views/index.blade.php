4b4af6a1d3d1360a893661ad29bf6b3dae1e436f
Johven <johven1984@hotmail.com>
2022-08-03 16:49:31 +0100

    Update title tag

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,11 +1,11 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <title>Genealogy Welcome</title>
+    <title>{{env('APP_NAME')}}</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    
+
     <!-- Bootstrap CSS v5.2.0-beta1 -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"  integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
@@ -20,24 +20,23 @@
         margin-top: 150px;
         font-size: 50px;
         color: green;
-    }   
-    
+    }
+
 </style>
 <body>
     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
     <!-- Bootstrap JavaScript Libraries -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
-    
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
-    
-    <h1 class="jumbotron text-center title font-weight-bold">Welcome to  
+
+    <h1 class="jumbotron text-center title font-weight-bold">Welcome to
       {{env('APP_NAME')}}.
     </h1>
-    
+
     <h4 class="mt-4 text-center container text-justify">
         Browser based Genealogy software for interacting and processing data efficiently. Easily create your own family tree by importing your existing data or manual data entry. Storage of all data is securely on your own server and does not leave your environment without your permission.</h4>
         <div class="text-center">
         </div></body>
         </html>
-        
-        
+
