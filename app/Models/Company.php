b5ccd7e61e0102d99c21ea43cf7fbff6f6d41ca9
Curtis Delicata <curtis.delicata@outlook.com>
2022-09-10 19:07:41 +0000

    Remove enso tenancy from company model

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -3,9 +3,7 @@
 namespace App\Models;
 
 use LaravelEnso\Companies\Models\Company as CoreCompany;
-use LaravelEnso\Multitenancy\Traits\SystemConnection;
 
 class Company extends CoreCompany
 {
-    use SystemConnection;
 }
