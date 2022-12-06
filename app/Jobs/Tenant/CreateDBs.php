84983e0f665f3703c681e23d53245ee4245882a0
Curtis Delicata <curtis.delicata@outlook.com>
2022-12-06 12:19:17 +0000

    Fix register job

--- app/Jobs/Tenant/CreateDBs.php
+++ app/Jobs/Tenant/CreateDBs.php
@@ -25,7 +25,7 @@ class CreateDBs implements ShouldQueue
      */
     public function __construct(Company $tenant)
     {
-        $this->tenany = $tenant;
+        $this->tenant = $tenant;
     }
 
     /**
