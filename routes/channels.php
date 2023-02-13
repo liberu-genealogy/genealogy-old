c6adda43efc31f1ad09077795861bef613d6f938
nikolaine <niko121879@gmail.com>
2023-02-13 12:12:26 -0800

    testevent and chat channel for test api

--- routes/channels.php
+++ routes/channels.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Broadcast;
 
 // Broadcast::channel('user.{user}', Notification::class);
-Broadcast::channel('App.Models.User.{id}', function($user, $id){
-    return (int) $user->id === (int) $id;
+Broadcast::channel('chat', function() {
+	return 'user';
 });
