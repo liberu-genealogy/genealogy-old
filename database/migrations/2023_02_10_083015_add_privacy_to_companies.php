4532ef7f51a3a72708b6b7eb36266602f908fd69
Josh Kisb <joshuakisb@gmail.com>
2023-05-26 14:53:54 +0300

    privacy nullable

--- database/migrations/2023_02_10_083015_add_privacy_to_companies.php
+++ database/migrations/2023_02_10_083015_add_privacy_to_companies.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('companies', function (Blueprint $table) {
-            $table->string('privacy');
+            $table->string('privacy')->nullable();
         });
     }
 
