38681314599284efe208a0c4ccdf669d8aea91dc
Josh Kisb <joshuakisb@gmail.com>
2023-06-01 12:11:25 +0300

    email primary password reset

--- database/migrations/tenant/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/tenant/2014_10_12_100000_create_password_resets_table.php
@@ -8,7 +8,7 @@ class CreatePasswordResetsTable extends Migration
     public function up()
     {
         Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
+            $table->string('email')->primary();
             $table->string('token')->index();
             $table->timestamp('created_at')->nullable();
         });
