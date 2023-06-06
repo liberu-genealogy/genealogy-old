107066f2f8ffa104fa7f06723555101cf81e2e45
Curtis Delicata <curtis.delicata@outlook.com>
2023-06-06 07:03:05 +0000

    Merge

--- app/Models/TenantPerson.php
+++ app/Models/TenantPerson.php
@@ -9,7 +9,6 @@ class TenantPerson extends Model
 {
     public $timestamps = false;
     protected $casts = [
-        'deleted_at' => 'datetime',
         'birthday' => 'datetime',
         'deathday' => 'datetime',
         'burial_day' => 'datetime',
@@ -24,7 +23,7 @@ class TenantPerson extends Model
         'phone', 'birthday', 'birth_month', 'birth_year', 'deathday', 'death_month', 'death_year',
         'burial_day', 'burial_month', 'burial_year', 'bank', 'bank_account', 'notes', 'gid', 'givn',
         'surn', 'uid', 'type', 'npfx', 'nick', 'spfx', 'nsfx', 'sex', 'description',
-        'child_in_family_id', 'deleted_at', 'chan', 'rin', 'resn', 'rfn', 'afn', 'birthday_dati',
+        'child_in_family_id','chan', 'rin', 'resn', 'rfn', 'afn', 'birthday_dati',
         'birthday_plac', 'deathday_dati', 'deathday_plac', 'deathday_caus', 'burial_day_dati',
         'burial_day_plac', 'famc', 'fams', 'titl', 'chr', 'created_at', 'updated_at',
     ];
