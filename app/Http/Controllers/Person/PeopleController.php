086c9ba0e5055780eea2ecf06f0e8b2eb887999b
Lukasz Tlalka <lukasz.tlalka@netblink.net>
2023-06-06 08:21:37 +0200

    Member tree search (refs: #2230)

--- app/Http/Controllers/Person/PeopleController.php
+++ app/Http/Controllers/Person/PeopleController.php
@@ -3,17 +3,31 @@
 namespace App\Http\Controllers\Person;
 
 use App\Http\Controllers\Controller;
-use App\Models\Person;
+use App\Models\TenantPerson;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class PeopleController extends Controller
 {
     public function searchPerson(Request $request)
     {
-        $authCode = $request->authcode;
-        $client = new \GuzzleHttp\Client();
-        $persons = Person::all();
+        // Don't show born under 100 years ago
+        // and filter living individuals out
+        $peopleQuery = TenantPerson::where("birthday", ">", Carbon::now()->subYears(100))
+            ->whereNotNull("deathday")
+            ->with([ "person" => function ($query) {
+                $query->select('id', 'name');
+            }])
+            ->limit(100);
 
-        return response()->json($persons);
+        foreach (explode(" ", $request->input('name')) as $text) {
+            $peopleQuery->where('name', 'like', '%'.$text."%");
+        }
+
+        $people = $peopleQuery->get();
+
+        return response()->json($people->map(function($person) {
+            return $person;
+        }));
     }
 }
