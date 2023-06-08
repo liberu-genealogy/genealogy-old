7e191f0d46d892a936366f07d513d6b447e70d63
star0713 <whitestar0713@gmail.com>
2023-06-08 18:17:28 +0800

    update api router

--- routes/api.php
+++ routes/api.php
@@ -1495,7 +1495,7 @@ Route::middleware(['auth', 'api'])
     ->group(function () {
         Route::get('get_companies', [CompanyIndex::class, 'getCompany']);
         Route::get('get_person', [PersonaliasIndex::class, 'getPerson']);
-        Route::post('gedcom-export', GedcomExport::class);
+        Route::get('gedcom-export', GedcomExport::class);
         Route::get('trees/options', [TreesManage::class, 'getOptions']);
     });
 
