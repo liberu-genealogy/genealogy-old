0ae662e00029a2447c2371d9c7c982a7def8358a
star0713 <whitestar0713@gmail.com>
2023-06-15 02:48:12 +0800

    verify stripe webhook request with security key

---
+++ app/Http/Middleware/VerifyStripeWebhook.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Stripe\WebhookSignature;
+use Stripe\Exception\SignatureVerificationException;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+
+class VerifyStripeWebhook
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * 
+     * @throws \Symfony\Component\HttpKernal\Exception\AccessDeniedHttpException
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        try {
+            WebhookSignature::verifyHeader(
+                $request->getContent(),
+                $request->header('Stripe-Signature'),
+                \Config::get('services.stripe.secret'),
+                null
+            );
+        } catch (SignatureVerificationException $exception) {
+            throw new AccessDeniedHttpException($exception->getMessage(), $exception);
+        }
+
+        return $next($request);
+    }
+}
